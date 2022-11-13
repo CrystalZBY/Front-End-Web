00ffe75d8013539432816da7922690d656b1b66f
CrystalZBY <113068907+CrystalZBY@users.noreply.github.com>
2022-11-13 12:25:16 -0500

    new changes

---
+++ Oct24/Ch4/script.js
@@ -0,0 +1,21 @@
+// CHAPTER 4 REPEAT STATEMENTS
+
+// While loop - repeats statements while a certain condition is true
+/* let number = 1;
+while (number <= 5){
+    console.log(number);
+    number++;
+}
+ */
+
+// For loop - able to manage what happens before loop starts and after each loop iteration runs
+/* let number;
+for (number = 1; number <=5; number++){
+    console.log(number);
+}
+
+for (let i = 1; i <= 5; i++){
+    console.log(i);
+} */
+
+// CODING TIME!
